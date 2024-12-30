db9d6e3bfffe2719edeaee6552b01b91d54394e5
chirag1020 <cbavaliya122@rku.ac.in>
2024-12-30 10:08:52 +0530

    new

--- chirag/src/lec2/statemgmt2.jsx
+++ chirag/src/lec2/statemgmt2.jsx
@@ -1,39 +1,48 @@
-import *as React from "react";
-class Statexyz extends React.Component {
-    state ={
-        First: false,
-        second : 3.14,
-        third : "Chirag",
-        fourth: "red",
-        fifth: "green",
-    };
-    constructor(){
-        super();
-        setTimeout(( ) => {
-            this.setState ({
-        First: false,
-        second : 4.28,
-        third : "Bavaliya",
-        fourth: "bule",
+import * as React from "react";
+class StateXYZ extends React.Component {
+  state = {
+    first: false,
+    second: 3.14,
+    third: "hello",
+    fourth: "red",
+    fifth: "green",
+  };
+  constructor() {
+    super();
+    setTimeout(() => {
+      this.setState({
+        first: true,
+        second: 4.28,
+        third: "world",
+        fourth: "blue",
         fifth: "purple",
-            });
-        },3000);
-
-    }
-    render(){
-        const {first, second,third} = this.state;
-        return(
-            <div>
-                <button disabled ={first}>My button</button>
-                <p style = {{ color: fourth}}>
-                    value of float variable is <strong>3.14</strong>
-                </p>
-                <p style = {{ color : fifth}}>
-                value of string  variable is <strong>Chirag</strong>
-                </p>
-            </div>
-        );
-    }
+      });
+    }, 3000);
+  }
+  render() {
+    const { first, second, third, fourth, fifth } = this.state;
+    return (
+      <div>
+        <button disabled={first}>My Button </button>
+        <p style={{ color: fourth }}>
+          Value of Float variable is <strong>{second}</strong>
+        </p>
+        <p style={{ color: fifth }}>
+          Value of String variable is<strong> {third}</strong>
+        </p>
+      </div>
+    );
+  }
 }
+export default StateXYZ;
 
-export default Statexyz;
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// // import StateABC from "./lec3012/statemgmt1";
+// import StateXYZ from "./lec3012/statemgmt2";
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <StateXYZ />
+//   </StrictMode>
+// );
--- chirag/src/main.jsx
+++ chirag/src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-//import StateABC from './lec2/statemgmt1' 
-import Statexyz from './lec2/statemgmt2';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import StateABC from "./lec2/statemgmt1";
+// import StateXYZ from "./lec2/statemgmt2";
+import StateMerge from "./lec2/statemgmt3";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <StateABC/>
-    <Statexyz/>
-  </StrictMode>,
+    <StateMerge/>
+  </StrictMode>
 );
